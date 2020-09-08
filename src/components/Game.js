d5d735e7108b29bf3dab4a98acc6b947059555f6
Prabin Parajuli <prabin.b9@gmail.com>
2020-09-08 16:06:13 +0200

    Update Game.js Handle Game Play

--- src/components/Game.js
+++ src/components/Game.js
@@ -11,4 +11,32 @@ export default class Game extends React.Component{
             ],
         };
     }
+    jumpTo(step){
+        this.setState({
+            stepNumber: step,
+            xIsNext: step % 2 == 0,
+        });
+    }
+    handleClick(i){
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+        const winner = calculateWinner(squares);
+        if(winner || squares[i]){
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X': 'O';
+        this.setState({
+            history: history.concat({
+                squares: squares,
+            }),
+            xIsNext: !this.state.xIsNext,
+            stepNumber: history.length.length,
+
+        });
+    }
+}
+
+function calculateWinner(squares){
+    return null;
 }
